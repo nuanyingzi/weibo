4e983cf477679d62ffe63a5f85c0ab03f0c3a315
tao <852947475@qq.com>
2025-03-27 15:17:20 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,20 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/app.css">
   <title>@yield('title', 'Weibo App') -- Laravel</title>
 </head>
 <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Weibo App</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
   @yield('content')
 </body>
 </html>
