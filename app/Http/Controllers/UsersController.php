5f10d0ca251d4e986c7e86af678dd3c1a47bb601
tao <852947475@qq.com>
2025-04-03 11:16:33 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,4 +17,17 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    public function store(Request $request)
+    {
+        $this->validate(
+            $request,
+            [
+                'name|姓名' => 'required|unique:users|max:60',
+                'email|电子邮箱' => 'required|email|unique:users|max:255',
+                'password|密码' => 'required|confirmed|min:6'
+            ]
+        );
+        halt($request);
+        return;
+    }
 }
