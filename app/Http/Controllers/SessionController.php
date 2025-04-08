75b6e78ccaf7b6e8ae3097fff20f3a9dbad627dc
tao <852947475@qq.com>
2025-04-08 15:28:48 +0800

    创建会话

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionController extends Controller
+{
+    // 登录页
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    // 登录
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt($credentials)) {
+            // 登录成功
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth()->user()]);
+        } else {
+            // 登录失败
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+    }
+}
