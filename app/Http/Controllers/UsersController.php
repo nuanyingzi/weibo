0933ed094eca1aea858e31da9a53c3cb450beaac
tao <852947475@qq.com>
2025-04-09 15:08:56 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,16 +7,19 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    // 注册页面
     public function create()
     {
         return view('users.create');
     }
 
+    // 用户详情页面
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
+    // 注册功能
     public function store(Request $request)
     {
         $this->validate(
@@ -36,5 +39,31 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '注册成功，欢迎开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    // 编辑页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    // 编辑功能
+    public function update(User $user, Request $request)
+    {
+        $this->validate(
+            $request,
+            [
+                'name' => 'required|max:60',
+                'password' => 'nullable|confirmed|min:6'
+            ]
+        );
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success', '个人资料更新成功');
+        return redirect()->route('users.show', $user->id);
     }
 }
