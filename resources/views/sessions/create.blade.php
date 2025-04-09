d3d1c75869e8cf1f6c43006957537b171a40eb0f
tao <852947475@qq.com>
2025-04-09 10:47:58 +0800

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
   <div class="offset-md-2 col-md-8">
-    <div class="card ">
+    <div class="card">
       <div class="card-header">
         <h5>登录</h5>
       </div>
@@ -13,14 +13,21 @@
         <form method="POST" action="{{ route('login') }}">
           {{ csrf_field() }}
 
-          <div class="mb-3">
+          <div class="form-group">
             <label for="email">邮箱：</label>
             <input type="text" name="email" class="form-control" value="{{ old('email') }}">
           </div>
 
-          <div class="mb-3">
+          <div class="form-group">
             <label for="password">密码：</label>
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+              <label for="exampleCheck1" class="form-check-label">记住我</label>
+            </div>
           </div>
 
           <button type="submit" class="btn btn-primary">登录</button>
